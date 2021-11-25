6c217304de7bdff9fbbee5459fb42a96655a1e36
jeroen de Bruin <j.debruin@e-village.nl>
2021-11-25 10:35:12 +0100

    Merge simple with configurable by default before sending the data to Canopy Deploy.

--- Clang/Model/Config/AttributeMappingConfigList.php
+++ Clang/Model/Config/AttributeMappingConfigList.php
@@ -7,7 +7,7 @@ use Magento\Framework\Data\OptionSourceInterface;
 class AttributeMappingConfigList implements OptionSourceInterface
 {
     const DEFAULT_VALUE = '1';
-    const MERGE_SIMPLE_WITH_CONFIGURABLE = '2';
+    const DO_NOT_MERGE_SIMPLE_WITH_CONFIGURABLE = '2';
 
     /**
      * @return array[]
@@ -16,7 +16,7 @@ class AttributeMappingConfigList implements OptionSourceInterface
     {
         return [
             ['value' => self::DEFAULT_VALUE, 'label' => __('Default')],
-            ['value' => self::MERGE_SIMPLE_WITH_CONFIGURABLE, 'label' => __('Merge simple with configurable')]
+            ['value' => self::DO_NOT_MERGE_SIMPLE_WITH_CONFIGURABLE, 'label' => __('Do not merge simple with configurable')]
         ];
     }
 }
